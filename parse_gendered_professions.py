fa1f8f728a16d60ff93c67a65b29e712f83fc68e
christoffer <csom@itu.dk>
2022-10-09 09:09:45 +0200

    add guidelines and labels for labeling

--- parse_gendered_professions.py
+++ parse_gendered_professions.py
@@ -1,3 +1,4 @@
+#!pip install pyarrow
 import pandas as pd
 import requests
 import re
@@ -12,35 +13,56 @@ from pathlib import Path
 #     .sort_values('Women')
 # )
 # df['Men'] = 100-df['Women']
-if not Path('gender_prof.parquet').exists():
-    URL = 'https://www.bls.gov/cps/cpsaat11.htm'
-    res = requests.get(URL)
-    res.raise_for_status()
+def query():
+    cache = 'gender_prof.parquet'
 
-    # For some reason, we get 2 duplicate dfs
-    txt = re.sub('<p class="sub(\d)">', r'<p>{\1}', res.text)
+    if not Path(cache).exists():
+        URL = 'https://www.bls.gov/cps/cpsaat11.htm'
+        res = requests.get(URL)
+        res.raise_for_status()
 
-    df, *rest = pd.read_html(txt, skiprows=2, attrs={'id': 'cps_eeann_emp_det_occu'})
-    assert not rest
-    # Cache as early as possible for flexibility
-    df.to_parquet('gender_prof.parquet') 
+        # Replace classes (with implicit/unreadable indentation)
+        # with explicit indentation class
+        txt = re.sub('<p class="sub(\d)">', r'<p>\1', res.text)
 
-df = pd.read_parquet('gender_prof.parquet')
+        df, *rest = pd.read_html(txt, skiprows=2, attrs={'id': 'cps_eeann_emp_det_occu'})
+        assert not rest
 
-totals = df.columns.get_level_values(1)
-assert '{0}Total, 16 years and over' in totals
+        # Cache as early as possible in preprocessing for flexibility
+        df.to_parquet(cache) 
 
-# Discard blank line and totals
-df.columns = df.columns.get_level_values(0) 
+    return pd.read_parquet(cache)
 
-df = (
-    df.replace('-', np.nan)
-    .dropna()
-    .sort_values('Women')
-    .iloc[:-1, :] # last row is a comment
-)
+def preprocessed():
+    df = query()
+    totals = df.columns.get_level_values(1)
+    assert '0Total, 16 years and over' in totals
 
+    # Discard blank line and totals
+    df.columns = df.columns.get_level_values(0) 
+
+    df = (
+        df
+        .iloc[:-1, :] # last row is a comment
+        .replace('-', np.nan)
+        .set_index('Occupation')
+        .astype(float)
+        .dropna()
+        .sort_values('Women')
+    )
+    return df
+
+
+
+
+df = preprocessed()
 level_0 = df[df.Occupation.str.startswith('{0}')]
 level_1 = df[df.Occupation.str.startswith('{1}')]
 level_2 = df[df.Occupation.str.startswith('{2}')]
 level_3 = df[df.Occupation.str.startswith('{3}')]
+
+raw_occupations = df.Occupation.str.lstrip('01234').to_list()
+with open('raw_occupations.txt', 'w') as f:
+    f.write('\n'.join(map(str, raw_occupations)))
+
+df.query('(Women < 0.2)')
---
+++ venv.sh
@@ -0,0 +1,3 @@
+# If it doesnt exist, go to home directory and type 
+# python -m venv labelstudio && source ~/labelstudio/bin/activate && pip install label-studio
+source ~/labelstudio/bin/activate
