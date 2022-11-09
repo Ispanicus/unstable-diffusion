7928304a773736b881ab6257dd118f3ec3c0ccca
christoffer <csom@itu.dk>
2022-11-09 09:33:24 +0100

    add flatten structure script

---
+++ text2imgdata_clean/flatten.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from shutil import copy
+
+this_dir = Path().resolve()
+assert this_dir.name == 'text2imgdata_clean'
+
+for dir in Path().glob('*'):
+    if not dir.is_dir():
+        continue
+    profession = dir.name
+    for i, file in enumerate(dir.glob('*')):
+        assert file.suffix == '.jpg'
+        copy(file, this_dir / f'{profession}_{i}.jpg')
+    break
