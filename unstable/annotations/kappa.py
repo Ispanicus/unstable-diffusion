cb029b7187b90cd6ee6cc7b6e0662be9c5b23e52
Johan Laursen <johan7laursen@gmail.com>
2022-11-16 09:05:06 +0100

    added api and non-api kappa-fleiss scores

--- unstable/annotations/kappa.py
+++ unstable/annotations/kappa.py
@@ -5,20 +5,33 @@ from unstable.annotations.load_anno_df import get_annotation_df
 
 df = get_annotation_df()
 
-pivot_df = (
-    df.drop(columns='profession')
-    .pivot(index='filename', columns='annotator', values=['annotation'])
-)
-pivot_df.columns = [author for annotation, author in pivot_df.columns]
+df_api, df_not_api = df.copy(), df.copy()
+df_api = df_api[df_api["api"]==True]
+df_not_api = df_not_api[df_not_api["api"]==False]
 
-print('Nulls per person')
-print(pivot_df.isnull().sum())
+def pivot_clean_agg(df,verbose=False):
+    pivot_df = (
+        df.drop(columns='profession')
+        .pivot(index='filename', columns='annotator', values=['annotation'])
+    )
+    pivot_df.columns = [author for annotation, author in pivot_df.columns]
 
-clean_label_df = np.array(pivot_df.dropna())
-agg_df, gender_categories = aggregate_raters(clean_label_df)
+    if verbose:
+        print('Nulls per person')
+        print(pivot_df.isnull().sum())
 
+    clean_label_df = np.array(pivot_df.dropna())
+    agg_df, gender_categories = aggregate_raters(clean_label_df)
+    return agg_df, gender_categories
+
+agg_df, _ = pivot_clean_agg(df,verbose=True)
+agg_df_api, _ = pivot_clean_agg(df_api)    
+agg_df_not_api, _ = pivot_clean_agg(df_not_api)
+    
 # https://en.wikipedia.org/wiki/Fleiss%27_kappa
-print(f'{fleiss_kappa(agg_df) = }')
+print(f'Full data: {fleiss_kappa(agg_df) = }')
+print(f'Api data: {fleiss_kappa(agg_df_api) = }')
+print(f'Non-Api data: {fleiss_kappa(agg_df_not_api) = }')
 
 annotation_ratios_per_prof = (
     df.groupby(["profession", "api"])
