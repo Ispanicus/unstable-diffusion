8f93eed7aad76bc62e25da4cd9d6b02c79d91727
christoffer <csom@itu.dk>
2022-11-18 21:13:29 +0100

    make gender profs loadable

--- unstable/occupations_and_prompts/prompter.py
+++ unstable/occupations_and_prompts/prompter.py
@@ -1,9 +1,13 @@
 import json
+from unstable.meta_tools import get_path
+
+def get_gender_occupations() -> dict:
+    return json.load(open(get_path('data/occupations.json')))
 
 PREFIX = 'A picture of a '
 
 if __name__ == '__main__':
-    gender_profs = json.load(open('occupations.json'))
+    gender_profs = get_gender_occupations()
 
     with open('prompts.txt', 'w') as f:
         for gender, profs in gender_profs.items():
