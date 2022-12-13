3ac2a34a5a7f1ab9832f907bd8209943edd3acac
christoffer <csom@itu.dk>
2022-12-13 09:43:21 +0100

    improve install script

--- install.bat
+++ install.bat
@@ -1,9 +1,11 @@
-git clone https://github.com/CompVis/stable-diffusion.git ../stable-diffusion
+python -m pip install pip -U
 
-conda env create -f ../stable-diffusion/environment.yaml
+git clone https://github.com/CompVis/stable-diffusion.git
+
+conda env create -f stable-diffusion/environment.yaml
 conda activate ldm
 
-python -m pip install pip -U
+python -m pip install -e stable-diffusion
 python -m pip install -e .
 
 :: %SystemRoot%\System32\cmd.exe /K "C:\Users\Christoffer\miniconda3\condabin\conda.bat activate ldm"
